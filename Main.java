5403e4a17aa384bf89868b0308379a6816f7ee3b
Noah Vargas Polo <noahvargaspolo73@gmail.com>
2026-06-18 09:31:33 -0700

    Printed results of real DACA data

--- Main.java
+++ Main.java
@@ -1,22 +1,43 @@
-// YOUR NAME: 
+// YOUR NAME: NOAH VARGAS POLO
 // COLLABORATORS:  
-// DATE: 
+// DATE: 6/18/26
 
 public class Main
 {
 	public static void main(String[] args)
 	{
 		// DECLARATION SECTION
+		double stateAvgCont;
+		int statAvgCountRounded;
+		String mostNumState;
+
+
+
 
 		// INITIALIZATION SECTION
-		
+		char stateMostDaca1 = 'c';
+		char stateMostDaca2 = 'a';
+		String stateMostDaca;
+		int numDacaRecipient = 700000;
+		long dacaFederalTaxRev = 60000000000L;
 		// INPUT SECTION
+
+
 		// N/A (no input for this lab)
 		
-		// CALCULATION & PROCESSING SECTION
 
-		
+		// CALCULATION & PROCESSING SECTION
+		stateAvgCont = ((double)dacaFederalTaxRev / numDacaRecipient);
+		statAvgCountRounded = (int)(stateAvgCont);
+		stateMostDaca1 = (char) (stateMostDaca1 - 32);
+		stateMostDaca2 = (char) (stateMostDaca2 - 32);
+		stateMostDaca = "" + stateMostDaca1 + stateMostDaca2;
 		// OUTPUT SECTION
-		
+		System.out.println("Average DACA-recipient Tax Contribution: "+stateAvgCont);
+		System.out.println("Rounded down to nearest whole dollar: "+statAvgCountRounded);
+		System.out.println("State with most DACA recipients: "+stateMostDaca);
+
+
+
 	}
 }
